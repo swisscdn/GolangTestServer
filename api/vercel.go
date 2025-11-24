62c486b439515c1c85c805c082c2bcacc7ae4824
Girish Bhutiya <girish.bhutiya2005@gmail.com>
2025-11-24 19:25:40 +0530

    test variable

--- api/vercel.go
+++ api/vercel.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 )
@@ -9,16 +10,19 @@ import (
 func Handler(w http.ResponseWriter, r *http.Request) {
 	// Get the TEST environment variable
 	testEnv := os.Getenv("TEST")
-	
+
 	// If not set, return a default message
 	if testEnv == "" {
 		testEnv = "TEST environment variable is not set"
+		log.Println("TEST environment variable is not set")
+	} else {
+		log.Println("TEST variable is:", testEnv)
 	}
-	
+
 	// Set response headers
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	
+
 	// Write the response
 	fmt.Fprintf(w, testEnv)
 }
