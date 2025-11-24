9a65fbd075922b9b907e6de57f5b471ca3c21301
Iris CDN Swiss GmbH. <64624702+swisscdn@users.noreply.github.com>
2025-11-24 18:17:13 +0530

    Update vercel.go

--- api/vercel.go
+++ api/vercel.go
@@ -1,67 +1,24 @@
-package api
+package handler
 
 import (
-	"log"
+	"fmt"
 	"net/http"
-
-	"github.com/swisscdn/OpenfiatServer/app/config"
-	db "github.com/swisscdn/OpenfiatServer/app/database"
-	"github.com/swisscdn/OpenfiatServer/app/handler"
-	"github.com/swisscdn/OpenfiatServer/app/routes"
-	"github.com/swisscdn/OpenfiatServer/app/token"
+	"os"
 )
 
-var (
-	server *handler.Server
-)
-
-// @title           Openflat Server API
-// @version         0.1
-// @description     This is a APIs for OpenflatServer.
-// @termsOfService
-
-// @contact.name   openfiat.org
-// @contact.url    #
-// @contact.email  support@openfiat.org
-
-// @license.name  Apache 2.0
-// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
-
-// @host      api.openfiat.org
-// @BasePath  /
-// @securityDefinitions.apikey Bearer
-// @in header
-// @name Authorization
-func init() {
-	con, err := config.LoadConfig(".")
-	if err != nil {
-		log.Fatal("can not load config:", err)
-	}
-	tokenMaker, err := token.NewPasetoMaker(con.TokenSymmetricKey)
-	if err != nil {
-		log.Fatalf("can not create toekn maker: %v", err)
-	}
-
-	//session, err := util.GetAstraDBSession(config)
-	client, err := config.GetAstraDBClient(con)
-	if err != nil {
-		log.Fatal("can not connect to DB:", err)
-	}
-
-	server = &handler.Server{
-		Config:     con,
-		TokenMaker: tokenMaker,
-		Store:      &db.DB{DB: client},
-	}
-	if con.DBMigrateUp {
-		db.MigrateUpDB(&db.DB{DB: client})
-	}
-	handler.InitServer(server)
-
-}
-
-// Entrypoint
 func Handler(w http.ResponseWriter, r *http.Request) {
-	//app.ServeHTTP(w, r)
-	routes.Routes(server.TokenMaker).ServeHTTP(w, r)
+	// Get the TEST environment variable
+	testEnv := os.Getenv("TEST")
+	
+	// If not set, return a default message
+	if testEnv == "" {
+		testEnv = "TEST environment variable is not set"
+	}
+	
+	// Set response headers
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	
+	// Write the response
+	fmt.Fprintf(w, testEnv)
 }
